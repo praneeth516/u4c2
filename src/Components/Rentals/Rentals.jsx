4cc8f8e4fa47160e6e02e5ad04bc71d5c0268fc8
Praneeth K <56603575+praneeth516@users.noreply.github.com>
2022-04-19 21:11:02 +0530

    first commit

--- src/Components/Rentals/Rentals.jsx
+++ src/Components/Rentals/Rentals.jsx
@@ -1,14 +1,53 @@
+import { useState, useEffect } from "react";
 import "./Rentals.css";
 
 export const Rentals = () => {
+  const [rentals, setrentals] = useState([])
+  const [rentalsort, setrentalsort] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:8080/houses").then((res) => res.json()).then((data) => { setrentals(data); setrentalsort(data) })
+  }, []);
+
+  const sortById = () => {
+    const data = rentalsort.sort(function (a, b) {
+      return a.id - b.id;
+    })
+    console.log(data);
+    setrentalsort(data)
+  }
+  const sortByAreaAsc = () => {
+    setrentalsort(rentals.sort(function (a, b) {
+      return a.areaCode - b.areaCode;
+    }))
+  }
+  const sortByAreaDesc = () => {
+    setrentalsort(rentals.sort(function (a, b) {
+      return a.areaCode + b.areaCode;
+    }))
+  }
+  const sortByRentAsc = () => {
+    setrentalsort(rentals.sort(function (a, b) {
+      return a.rent - b.rent;
+    }))
+
+  }
+  const sortByRentDesc = () => {
+    setrentalsort(rentals.sort(function (a, b) {
+      return a.rent + b.rent;
+    }))
+
+  }
+
   return (
     <div className="rentalContainer">
+      {console.log(rentalsort)}
       <div className="sortingButtons">
-        <button className="sortById">Sort by ID</button>
-        <button className="sortByRentAsc">Rent Low to high</button>
-        <button className="sortByRentDesc">Rent High to low</button>
-        <button className="sortByAreaAsc">Area Low to high</button>
-        <button className="sortByAreaDesc">Area High to Low</button>
+        <button className="sortById" onClick={sortById}>Sort by ID</button>
+        <button className="sortByRentAsc" onClick={sortByRentAsc}>Rent Low to high</button>
+        <button className="sortByRentDesc" onClick={sortByRentDesc}>Rent High to low</button>
+        <button className="sortByAreaAsc" onClick={sortByAreaAsc}>Area Low to high</button>
+        <button className="sortByAreaDesc" onClick={sortByAreaDesc}>Area High to Low</button>
       </div>
       <input
         className="searchAddress"
@@ -29,7 +68,7 @@ export const Rentals = () => {
           </tr>
         </thead>
         <tbody>
-          {[].map((house, index) => {
+          {rentalsort.map((house, index) => {
             return (
               <tr key={house.id} className="houseDetails">
                 <td className="houseId">{house.id}</td>
@@ -39,7 +78,7 @@ export const Rentals = () => {
                 <td className="areaCode">{house.areaCode}</td>
                 <td className="rent">{house.rent}</td>
                 <td className="preferredTenants">
-                  {/* Show text Both or Bachelors or Married based on values */}
+                  {house.preferredTenants}
                 </td>
                 <td className="houseImage">
                   <img src={house.image} alt="house" />
