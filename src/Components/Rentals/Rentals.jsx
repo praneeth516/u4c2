bcb045ca17b453db727f8b8f483823133d383f9f
Praneeth K <56603575+praneeth516@users.noreply.github.com>
2022-04-19 23:49:35 +0530

    Saving to Github

--- src/Components/Rentals/Rentals.jsx
+++ src/Components/Rentals/Rentals.jsx
@@ -10,38 +10,38 @@ export const Rentals = () => {
   }, []);
 
   const sortById = () => {
-    const data = rentalsort.sort(function (a, b) {
+    const data = rentals.sort(function (a, b) {
       return a.id - b.id;
     })
-    console.log(data);
-    setrentalsort(data)
+    setrentalsort([...data])
   }
   const sortByAreaAsc = () => {
-    setrentalsort(rentals.sort(function (a, b) {
+    const data = rentals.sort(function (a, b) {
       return a.areaCode - b.areaCode;
-    }))
+    })
+    setrentalsort([...data])
   }
   const sortByAreaDesc = () => {
-    setrentalsort(rentals.sort(function (a, b) {
-      return a.areaCode + b.areaCode;
-    }))
+    const data = rentals.sort(function (a, b) {
+      return b.areaCode - a.areaCode;
+    })
+    setrentalsort([...data])
   }
   const sortByRentAsc = () => {
-    setrentalsort(rentals.sort(function (a, b) {
+    const data = rentals.sort(function (a, b) {
       return a.rent - b.rent;
-    }))
-
+    })
+    setrentalsort([...data])
   }
+
   const sortByRentDesc = () => {
-    setrentalsort(rentals.sort(function (a, b) {
-      return a.rent + b.rent;
-    }))
-
+    const data = rentals.sort(function (a, b) {
+      return b.rent - a.rent;
+    })
+    setrentalsort([...data])
   }
-
   return (
     <div className="rentalContainer">
-      {console.log(rentalsort)}
       <div className="sortingButtons">
         <button className="sortById" onClick={sortById}>Sort by ID</button>
         <button className="sortByRentAsc" onClick={sortByRentAsc}>Rent Low to high</button>
@@ -68,26 +68,24 @@ export const Rentals = () => {
           </tr>
         </thead>
         <tbody>
-          {rentalsort.map((house, index) => {
-            return (
-              <tr key={house.id} className="houseDetails">
-                <td className="houseId">{house.id}</td>
-                <td className="houseName">{house.name} </td>
-                <td className="ownersName">{house.ownerName}</td>
-                <td className="address">{house.address}</td>
-                <td className="areaCode">{house.areaCode}</td>
-                <td className="rent">{house.rent}</td>
-                <td className="preferredTenants">
-                  {house.preferredTenants}
-                </td>
-                <td className="houseImage">
-                  <img src={house.image} alt="house" />
-                </td>
-              </tr>
-            );
-          })}
+          {rentalsort.map((house, index) => (
+            <tr key={house.id} className="houseDetails">
+              <td className="houseId">{house.id}</td>
+              <td className="houseName">{house.name} </td>
+              <td className="ownersName">{house.ownerName}</td>
+              <td className="address">{house.address}</td>
+              <td className="areaCode">{house.areaCode}</td>
+              <td className="rent">{house.rent}</td>
+              <td className="preferredTenants">
+                {house.preferredTenants}
+              </td>
+              <td className="houseImage">
+                <img src={house.image} alt="house" />
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
   );
-};
+}
