85352a866aa312fcdf63717c45db6d99d9dced00
Praneeth K <56603575+praneeth516@users.noreply.github.com>
2022-04-20 13:53:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,22 +5,26 @@ import { useState } from "react";
 
 function App() {
     const [form, setform] = useState(false)
-
-    const changehandler = () => {
+    const changlehandler = () => {
         setform((prev) => !prev)
+    }
+    const formsubmit = () => {
+        setform(false);
     }
     return (
         <div className="App">
-            <button className="toggleForm" onClick={changehandler}>
+            <button className="toggleForm" onClick={changlehandler}>
                 {/* Show text Add House or Show Rentals based on state */}
                 {form ? "Show Rentals" : "Add House"}
             </button>
-            {/* Show component> based on state */}
-            {form && <AddHouse />}
+            {/* Show component based on state */}
             {!form && <Rentals />}
-
+            {form && <AddHouse submit={formsubmit} />}
+            <br />
         </div>
     );
 }
 
 export default App;
+
+
